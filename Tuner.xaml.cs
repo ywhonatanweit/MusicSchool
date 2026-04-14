f42e4e8edffc3a6f7eeabdb28e44d54cdde1e5c3
viterman <yweith2008@gmail.com>
2026-04-14 11:02:18 +0300

    start git

--- Tuner.xaml.cs
+++ Tuner.xaml.cs
@@ -79,6 +79,8 @@
 //}
 using System.Windows;
 using System.Windows.Controls;
+using NAudio.Wave;
+
 
 namespace MusicSchoolWpf
 {
@@ -133,6 +135,107 @@ namespace MusicSchoolWpf
 
             else
                 AcousticLeft.Visibility = Visibility.Visible;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void he(object sender, RoutedEventArgs e)
+        {
+            frequency.Text = "329.6";
+        }
+
+        private void d(object sender, RoutedEventArgs e)
+        {
+            frequency.Text = "146.8";
+
+        }
+
+        private void g(object sender, RoutedEventArgs e)
+        {
+            frequency.Text = "196.0";
+
+        }
+
+        private void a(object sender, RoutedEventArgs e)
+        {
+            frequency.Text = "110.0";
+
+        }
+
+        private void b(object sender, RoutedEventArgs e)
+        {
+            frequency.Text = "246.9";
+        }
+
+        private void le(object sender, RoutedEventArgs e)
+        {
+            frequency.Text = "82.4";
+
+        }
+
+        private WaveInEvent waveIn;
+        private float[] buffer = new float[2048];
+
+        private float targetFrequency = 82.4f; // אפשר לשנות בעתיד
+        private float tolerance = 5f; // טווח ירוק (Hz)
+
+        
+
+        private void StartListening()
+        {
+            waveIn = new WaveInEvent();
+            waveIn.WaveFormat = new WaveFormat(44100, 1);
+            waveIn.DataAvailable += OnDataAvailable;
+            waveIn.StartRecording();
+            frequency.Text = float.Parse(DetectFrequency);
+        }
+
+        private void OnDataAvailable(object sender, WaveInEventArgs e)
+        {
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                buffer[i] = BitConverter.ToInt16(e.Buffer, i * 2) / 32768f;
+            }
+
+            float freq = DetectFrequency(buffer, 44100);
+
+            Dispatcher.Invoke(() => UpdateUI(freq));
+        }
+
+        private float DetectFrequency(float[] samples, int sampleRate)
+        {
+            int crossings = 0;
+
+            for (int i = 1; i < samples.Length; i++)
+            {
+                if (samples[i - 1] < 0 && samples[i] >= 0)
+                    crossings++;
+            }
+
+            return crossings * sampleRate / (2f * samples.Length);
+        }
+
+        private void UpdateUI(float frequency)
+        {
+            // כמה רחוק מהתדר הרצוי
+            double diff = frequency - targetFrequency;
+
+            // הגבלת טווח תצוגה
+            double maxRange = 20; // Hz
+            diff = Math.Max(-maxRange, Math.Min(maxRange, diff));
+
+            // המרה למיקום (פיקסלים)
+            double pixelsPerHz = 5;
+            double offset = diff * pixelsPerHz;
+
+            // הזזת הקו הלבן
+            FrequencyLine.Margin = new Thickness(offset, 0, 0, 0);
+
+            // עדכון רוחב האזור הירוק לפי tolerance
+            GoodRange.Width = tolerance * pixelsPerHz * 2;
         }
     }
 }
